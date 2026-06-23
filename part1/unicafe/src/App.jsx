189d0675f7b006f37845c4bd9788c492f541519b
Sara <leungyunyee@gmail.com>
2026-06-23 15:52:40 +0800

    exercise 1.9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,25 @@
 import { useState } from 'react'
 
+const Statistics = (props) =>{
+  if(props.total===0)
+    return(
+      <>
+        <h1>Statistics</h1>
+        <p>No feedback gathered</p>
+      </>
+    )
+  return(
+    <>
+      <h1>Statistics</h1>
+      <p>Good {props.good} </p>
+      <p>Neutral {props.neutral} </p>
+      <p>Bad {props.bad} </p>
+      <p>all {props.total}</p>
+      <p>average {props.total === 0 ? 0 : props.average}</p>
+    </>
+  )
+
+}
 
 const Button = (props) => (
   <button onClick={props.onClick}>
@@ -8,27 +28,34 @@ const Button = (props) => (
 )
 
 const App = () => {
-  const [goodCount, setGoodCount] = useState(0)
-  const [neutralCount, setNeutralCount] = useState(0)
-  const [badCount, setBadCount] = useState(0)
+  const [goodCount, setGoodCount] = useState(0);
+  const [neutralCount, setNeutralCount] = useState(0);
+  const [badCount, setBadCount] = useState(0);
+  const [total, setTotal] = useState(0);
 
-  const addGoodCount = () =>{setGoodCount(goodCount+1)}
-  const addNeutralCount = () =>{setNeutralCount(neutralCount+1)}
-  const addBadCount = () =>{setBadCount(badCount+1)}
+  const addGoodCount = () => {
+    setGoodCount(goodCount+1),
+    setTotal(total+1)
+  }
+  const addNeutralCount = () => {
+    setNeutralCount(neutralCount+1),
+    setTotal(total+1)
+  }
+  const addBadCount = () => {
+    setBadCount(badCount+1),
+    setTotal(total+1)
+  }
+
+  const average = total === 0 ? 0 : (goodCount - badCount) / total;
 
 
   return (
     <>
       <h1>give feedback</h1>
-      <div>
-        <Button onClick={()=>addGoodCount()} text="good" />
-        <Button onClick={()=>addNeutralCount()} text="neutral" />
-        <Button onClick={()=>addBadCount()} text="bad" />
-      </div>
-      <h1>Statistics</h1>
-      <p>Good {goodCount} </p>
-      <p>Neutral {neutralCount} </p>
-      <p>Bad {badCount} </p>
+      <Button onClick={addGoodCount} text="good" />
+      <Button onClick={addNeutralCount} text="neutral" />
+      <Button onClick={addBadCount} text="bad" />
+      <Statistics good={goodCount} neutral={neutralCount} bad={badCount} average={average} total={total} />
     </>
 
   )
